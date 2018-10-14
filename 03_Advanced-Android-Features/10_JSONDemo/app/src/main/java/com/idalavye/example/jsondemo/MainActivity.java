d1ec16145eb622e496745c09bb543956f5bc3312
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2018-10-14 15:39:57 +0300

    JSON Demo app added

---
+++ 03_Advanced-Android-Features/10_JSONDemo/app/src/main/java/com/idalavye/example/jsondemo/MainActivity.java
@@ -0,0 +1,87 @@
+package com.idalavye.example.jsondemo;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            //Background'da işlem yaparken UI elemanlarını kesinlikle etkilememelidir.
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection;
+
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return e.toString();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String weatherInfo = jsonObject.getString("weather");
+
+                JSONArray arr = new JSONArray(weatherInfo);
+
+                for (int i = 0; i < arr.length(); i++) {
+                    JSONObject jsonObject1 = arr.getJSONObject(i);
+
+                    Log.i("main",jsonObject1.getString("main"));
+                    Log.i("description",jsonObject1.getString("description"));
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            //Log.i("JSON", s);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        try {
+            task.execute("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22").get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
