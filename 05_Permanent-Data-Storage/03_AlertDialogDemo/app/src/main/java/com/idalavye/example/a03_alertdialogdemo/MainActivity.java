7bec92c899224e730c749754c638a064577d8026
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2018-10-21 15:26:25 +0300

    alertDialog sample added

---
+++ 05_Permanent-Data-Storage/03_AlertDialogDemo/app/src/main/java/com/idalavye/example/a03_alertdialogdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.idalavye.example.a03_alertdialogdemo;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are you sure?")
+                .setMessage("Do yo definitely want to do this")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "It's Done!", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+}
